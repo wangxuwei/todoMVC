b9aaf5145be1bc5de3354240787a97bd7d1272e7
wangxuwei <wangxuwei84@gmail.com>
2016-12-21 10:55:01 +0800

    . add custom pusher for textarea if description undefined

--- src/js-app/main.js
+++ src/js-app/main.js
@@ -1,5 +1,9 @@
 var d = mvdom;
 
+d.pusher("textarea", function(value){
+	this.value = value || "";
+});
+
 document.addEventListener("DOMContentLoaded", function(event) {
 	d.display("MainView", d.first("body"));
 });
